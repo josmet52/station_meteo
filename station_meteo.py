432c869ae79732aefafd767068f522c2e06dfbfc
josmet52 <joseph.metrailler@bluewin.ch>
2017-07-09 11:03:34 +0200

    wu201707091102

--- station_meteo.py
+++ station_meteo.py
@@ -4,7 +4,11 @@ import urllib2
 import json
 import requests
 
-f =requests.get ('http://api.wunderground.com/api/67b3e8dedd282c83/forecast/q/CH/Sion.json')
-data = f.json
-print data["forecast"]["simpleforecast"]
+r = requests.get("http://api.wunderground.com/api/67b3e8dedd282c83/forecast10day/q/CH/Sion.json")
+data = r.json()
+
+for day in data['forecast']['simpleforecast']['forecastday']:
+    print day['date']['weekday'] + ' ' + str(day['date']['day']) + " : " + day['conditions']
+    print "High: ", day['high']['celsius'] + "C", "Low: ", day['low']['celsius'] + "C", ''
+    print
 
