2adb62e4a1078348c4a48bb7226c685a5c9ccfaf
josmet52 <joseph.metrailler@bluewin.ch>
2017-08-10 17:38:11 +0200

    avec commentaires

---
+++ station_meteo_save_json.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2
+import json
+import requests
+import time
+import Adafruit_CharLCD as LCD
+import unicodedata
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
+lcd = LCD.Adafruit_CharLCDPlate()
+lcd.clear()
+
+##sPays = "ES"
+##sVille = "la Garriga"
+##sLang = "lang:SP"
+sPays = "CH"
+sVille = "Sion"
+sLang = "lang:FR"
+##sPays = "IT"
+##sVille = "Todi"
+##sLang = "lang:IT"
+sKey = "67b3e8dedd282c83"
+sDemand = "forecast10day"
+sUrl = "http://api.wunderground.com/api"
+sRequest = sUrl + "/" + sKey + "/" + sDemand + "/" + sLang + "/q/" + sPays + "/" + sVille +".json"
+sTooLong =["Partiellement", "Parzialmente", "Possibilita", "Parcialmente", "Probabilidad"]
+
+print "started"
+r = requests.get(sRequest)
+data = r.json()
+print data
+
+# Open a file in witre mode
+fo = open("xml_jo.txt", "rw+")
+line = fo.writelines( data )
+fo.close()
+print "finished"
