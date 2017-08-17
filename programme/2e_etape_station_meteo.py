6e29e2440fb9a6077f746e0efeb81082ce03a034
josmet52 <joseph.metrailler@bluewin.ch>
2017-08-17 21:18:00 +0200

    avec 2e etape avec script

--- programme/2e_etape_station_meteo.py
+++ programme/2e_etape_station_meteo.py
@@ -16,6 +16,11 @@ Ce programme interroge le site internet www.wunderground.com/api pour obtenir le
 # importation des librairies nécessaires
 import json
 import requests
+import Adafruit_CharLCD as LCD
+
+
+# assignation du display à la variable lcdDisplay 
+lcdDisplay = LCD.Adafruit_CharLCDPlate()
 
 #---------------------------------------------------------------------------------------------------------------------------------
 # interrogation du site wunderground,com
@@ -26,11 +31,16 @@ sForecast = r.json()
 # boucle pour parcourir les prévisions jour (day) après jour (4 jours pour forecast)
 for day in sForecast['forecast']['simpleforecast']['forecastday']:
 
-  print "Prevision meteo pour Sion le " + day['date']['weekday_short'] + " " + str(day['date']['day']) + " " + day['date']['monthname_short'] + " " + str(day['date']['year'])
-  print "Temperature min prevue : " + day['low']['celsius']
-  print "Temperature max prevue : " + day['high']['celsius']
-  print "Prevision meteo : " + day['conditions']
-  print "-----------------------------------------------------"
+    print "Prevision meteo pour Sion le " + day['date']['weekday_short'] + " " + str(day['date']['day']) + " " + day['date']['monthname_short'] + " " + str(day['date']['year'])
+    print "Temperature min prevue : " + day['low']['celsius']
+    print "Temperature max prevue : " + day['high']['celsius']
+    print "Prevision meteo : " + day['conditions']
+    print "-----------------------------------------------------"
 
+    str1 = day['date']['weekday_short'] + " " + str(day['date']['day']) + " -> " + day['low']['celsius'] + "-" + day['high']['celsius']
+    str2 = day['conditions']
+    lcdDisplay.clear()
+    lcdDisplay.message(str1 + "\n" + str2)
+    
 # Fin du programme
 #---------------------------------------------------------------------------------------------------------------------------------
